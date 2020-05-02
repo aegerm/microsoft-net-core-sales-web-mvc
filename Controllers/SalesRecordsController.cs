b4216df694f94b539c2ce8a4f8952f8e0b10681c
Alexandre Eger Marques <alexandreegermarques@gmail.com>
2020-05-02 17:13:13 -0300

    Implementado página de relatório de vendas.

---
+++ Controllers/SalesRecordsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sales_web_mvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
